0d2616515996aeb0f594adc2416f2936bdd98682
iceymoss <ice_moss@163.com>
2024-03-04 23:21:06 +0800

    laravel学习

---
+++ laravel/test-app/app/Http/Controllers/MemberController.php
@@ -0,0 +1,14 @@
+<?php
+
+//命名空间
+namespace App\Http\Controllers;
+
+class MemberController extends Controller
+{
+    public function Info()
+    {
+        return "{'name': 'iceymoss', 'age': 18}";
+    }
+}
+
+
---
+++ laravel/test-app/routes/web.php
@@ -0,0 +1,58 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+//get
+Route::get("/hello", function () {
+    return "hello, iceymoss!";
+});
+
+//post
+Route::post("/getUserInfo", function () {
+    return "this is a post request";
+});
+
+//match
+Route::match(["get", "post"], 'match', function () {
+    return "this is a match request";
+});
+
+//any
+Route::any("getAny", function () {
+    return "this is a any request";
+});
+
+//获取参数， 在path中获取参数
+Route::get("user/{id}", function ($id) {
+    return "user-". $id;
+});
+
+//路由分组
+Route::group(["prefix" => "user"], function() {
+    Route::get("list", function () {
+        return json_encode([["name" => "iceymoss1", "age" => 18, "gender" => 1], "name" => "iceymoss10", "age" => 23, "gender" => 1, "name" => "iceymoss34", "age" => 17, "gender" => 1]);
+    });
+
+    Route::get("{id}", function ($id) {
+        return $id;
+    });
+});
+
+//输出视图
+Route::get("/welcome", function () {
+    return view("welcome");
+});
+
+Route::get("member/info", 'MemberController@Info');
+
